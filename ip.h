497e9d8e690519ca40c156584ddd4ef3b698d05d
lqzzuu88 <lqzzuu88@outlook.com>
2017-08-28 11:44:46 +0800

    update ip.h

--- ip.h
+++ ip.h
@@ -2,10 +2,13 @@
 #define IP_H
 #include <QString>
 
+//local machine name
 QString get_localmachine_name();
 
+//local ip
 QString get_localmachine_ip();
 
+//local mac address
 QString get_localmachine_mac(QString ip);
 
 #endif // IP_H
