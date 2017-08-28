356860de61c7d77347597460a84d1ef1de1915c3
lqzzuu88 <lqzzuu88@outlook.com>
2017-08-28 13:56:21 +0800

    add you see to ip.h

--- ip.h
+++ ip.h
@@ -8,7 +8,7 @@ QString get_localmachine_name();
 //local ip
 QString get_localmachine_ip();
 
-//local mac address, it depend on ip
+//local mac address, it depend on ip, you see
 QString get_localmachine_mac(QString ip);
 
 #endif // IP_H
